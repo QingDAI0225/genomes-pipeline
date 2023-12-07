912eebaaddc3b82318b6da9cfdf9dcd1293ef663
QingDAI0225 <55598949+QingDAI0225@users.noreply.github.com>
2023-12-07 13:22:26 -0500

    Update download_database.sh
    revise amrfinder download error

--- download_database.sh
+++ download_database.sh
@@ -32,8 +32,10 @@ wget ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/genomes-pipeline/kegg_classe
 wget ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/genomes-pipeline/continent_countries.csv
 # gtdb
 wget https://data.ace.uq.edu.au/public/gtdb/data/releases/release214/214.0/auxillary_files/gtdbtk_r214_data.tar.gz
+
 #amrfinder
-wget ftp://ftp.ncbi.nlm.nih.gov/pathogen/Antimicrobial_resistance/AMRFinderPlus/database/3.11/2023-02-23.1
+wget -r -np -R "index.html*" ftp://ftp.ncbi.nlm.nih.gov:21/pathogen/Antimicrobial_resistance/AMRFinderPlus/database/3.11/2023-02-23.1/
+
 # InterProScan5.62-94.0
 wget ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/5.36-75.0/interproscan-5.62-94.0.tar.gz
 tar --extract --gzip interproscan-5.62-94.0.tar.gz
